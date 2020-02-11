8d12ffa4c40d4f82f2bac9d62a3c3105290ff171
Victor Sera <sera.victor6023@gmail.com>
2020-02-11 23:00:34 +0200

    Starting to prettify join-request management

--- src/app/requests/requests.component.ts
+++ src/app/requests/requests.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JoinRequest } from '../classes/request';
+import { ParticipationService } from '../service/participation.service';
+import { AuthenticationService } from '../service/authentication.service';
+import { User } from '../service/data/user-data.service';
 
 @Component({
   selector: 'app-requests',
@@ -11,9 +14,41 @@ export class RequestsComponent implements OnInit {
   // private requests: JoinRequest[]
   private requests: JoinRequest[] = []
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private participationService: ParticipationService
+  ) { }
 
   ngOnInit() {
+    this.refresh();
+  }
+
+  refresh() {
+    this.authenticationService.getLoggedInUser().subscribe(
+      user => {
+        this.getIncomingRequestsForUser(user);
+      }
+    )
+  }
+
+  getIncomingRequestsForUser(user: User) {
+    this.participationService.getIncomingRequests(user.id).subscribe(
+      requests => this.requests = requests
+    )
+  }
+
+  acceptRequest(userId: number, tripId: number) {
+    console.log('Accept');
+    this.participationService.acceptRequest(userId, tripId).subscribe(
+      response => this.refresh()
+    );
+  }
+
+  declineRequest(userId: number, tripId: number) {
+    console.log('Decline');
+    this.participationService.declineRequest(userId, tripId).subscribe(
+      response => this.refresh()
+    );
   }
 
 }
--- src/app/service/participation.service.ts
+++ src/app/service/participation.service.ts
@@ -3,12 +3,13 @@ import { User } from './data/user-data.service';
 import { Trip } from '../list-trips/list-trips.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { JoinRequest } from '../classes/request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParticipationService {
-
+  
   constructor(
     private http: HttpClient
   ) { }
@@ -23,5 +24,17 @@ export class ParticipationService {
 
   getStatus(user: User, trip: Trip) {
     return this.http.get(`${environment.apiUrl}/participations/${user.id}/${trip.id}`);
+  }
+
+  getIncomingRequests(userId: number) {
+    return this.http.get<JoinRequest[]>(`${environment.apiUrl}/users/${userId}/in-requests`);
+  }
+
+  acceptRequest(userId: number, tripId: number) {
+    return this.http.put(`${environment.apiUrl}/participations/${userId}/${tripId}/accept`, {})
+  }
+
+  declineRequest(userId: number, tripId: number) {
+    return this.http.put(`${environment.apiUrl}/participations/${userId}/${tripId}/decline`, {})
   }
 }
